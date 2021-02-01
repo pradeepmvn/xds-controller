0f865437e3a848bef1fe3d394eae2edf199290c0
pxm286 <pradeep.mamillapalli@equifax.com>
2021-01-31 19:47:20 -0500

    docs: log docs and image update

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -7,11 +7,15 @@ import (
 )
 
 var (
-	Info  *log.Logger
+	// Info level
+	Info *log.Logger
+	// Debug level
 	Debug *log.Logger
+	// Error level
 	Error *log.Logger
 )
 
+//CLog control-plane snapshot logger
 type CLog struct{}
 
 // NewLogger instantiates instance var with the configurations
@@ -28,22 +32,26 @@ func NewLogger(debugFlag bool) {
 
 	// instatiate package level vars
 	Debug = log.New(debugHandle, "DEBUG: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC)
-	Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
-	Error = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
+	Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lmicroseconds)
+	Error = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lmicroseconds)
 }
 
+//Debugf Logger for controlplane, funcs used in snapshot logging
 func (c CLog) Debugf(format string, args ...interface{}) {
 	Debug.Printf(format, args...)
 }
 
-func (logger CLog) Infof(format string, args ...interface{}) {
+//Infof Logger for controlplane, funcs used in snapshot logging
+func (c CLog) Infof(format string, args ...interface{}) {
 	Info.Printf(format, args...)
 }
 
-func (logger CLog) Warnf(format string, args ...interface{}) {
+//Warnf Logger for controlplane, funcs used in snapshot logging
+func (c CLog) Warnf(format string, args ...interface{}) {
 	Info.Printf(format, args...)
 }
 
-func (logger CLog) Errorf(format string, args ...interface{}) {
+//Errorf Logger for controlplane, funcs used in snapshot logging
+func (c CLog) Errorf(format string, args ...interface{}) {
 	Error.Printf(format, args...)
 }
